c71c8685f460d50d655816ac9705881170e8943d
Mbaoma <mmaryraphaella@gmail.com>
2023-01-06 22:18:52 +0100

    add /people endpoint and its test case

--- Part_1/tests/test_project1.py
+++ Part_1/tests/test_project1.py
@@ -6,3 +6,8 @@ def test_homepage_route_status_code(app, client):
     route = "/"
     rv = client.get(route)
     assert rv.status_code == 200
+
+def test_people_page_status_code(app, client):
+    route = "/api/people"
+    rv = client.get(route)
+    assert rv.status_code == 200
